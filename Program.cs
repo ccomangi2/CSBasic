73fc2f439d4f14a917d6240f32f84f6a4767d8e1
suvin <ccomangi2@gmail.com>
2021-04-21 08:58:43 +0900

    초기 커밋.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CSBasic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello C# Progeamming ..!");
+            var name = "이" + "수" + "빈";
+            Console.Write("개행이 안되는 녀석");
+            Console.WriteLine("개행이 되는 녀석");
+
+            var alpha = "알파";
+            // var 273alpha = "숫자이름";
+            var 한글변수명 = "된당";
+            // string 이모지 = "이모지는 안 된다";
+
+            // 인라인 주석
+            /*
+             * 여러 줄 주석
+             */
+
+            Console.WriteLine(52);
+            // Arithmetic Opertor + - * /
+            Console.WriteLine(52 + 273);
+            // Opertor
+            Console.WriteLine(5 + 3 * 2);
+            Console.WriteLine(10 / 5);
+            Console.WriteLine(10 % 5);
+            Console.WriteLine(7 / 3);
+        }
+    }
+}
