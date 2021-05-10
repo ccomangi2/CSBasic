5fd10f77e887854868e362b701409a4d6da298bf
Lee Su Bin <ccomangi2@gmail.com>
2021-05-10 13:18:33 +0900

    13-3. 중첩 조건문

--- Program.cs
+++ Program.cs
@@ -289,6 +289,23 @@ namespace CSBasic
             {
                 Console.WriteLine("홀수입니다!");
             }
+            
+            //중첩 조건문
+            if(DateTime.Now.Hour < 11)
+            {
+                Console.WriteLine("아침 먹을 시간입니다.");
+            }
+            else
+            {
+                if(DateTime.Now.Hour < 15)
+                {
+                    Console.WriteLine("점심 먹을 시간입니다.");
+                }
+                else
+                {
+                    Console.WriteLine("저녁 먹을 시간입니다.");
+                }
+            }
         }
     }
 }
